7da5884b3697ebc44a1bcca9157e6bf7c80c86fb
Mahi <mahigadi27@gmail.com>
2025-10-01 00:41:20 +0530

    removed files

--- build/gps_frontier_explorer/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mahi/gps_frontier_explorer/exploring_exploration/install/gps_frontier_explorer'
